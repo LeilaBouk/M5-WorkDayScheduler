c302448b0fc17416d264cfd2bd8a84987032dd1c
Leila <leila.boukella@gmail.com>
2022-08-12 18:27:47 -0700

    started save function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,13 @@
+// Get the current date
 var now = moment().format("dddd, MMMM Do");
 $("#currentDay").text(now);
+
+
+var save = $("#save");
+
+// save user text input
+function saveSchedule() {
+    console.log("save");
+} 
+
+save.on("click", saveSchedule);
