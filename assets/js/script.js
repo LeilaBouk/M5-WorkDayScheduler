7cee9ed1ba55381ff60101dc4ddac26dad8f9099
Leila <leila.boukella@gmail.com>
2022-08-13 14:34:11 -0700

    working on block colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,33 @@
 var now = moment().format("dddd, MMMM Do");
 $("#currentDay").text(now);
 
+// Check the current time
+var timeCheck = moment().format("HH");
 
+var timeBlock = $(".row");
+
+actTime = (timeBlock.attr("id"));
+
+console.log(actTime);
+console.log(timeCheck);
+
+function blockColor() {
+    if (actTime > timeCheck) {
+        timeBlock.addClass("past");
+    }
+}
+
+blockColor();
+
+
+// Get the save button
 var save = $(".save");
 
 // save user text input
 function saveSchedule() {
     console.log(this);
-    $(this).parent().hide();
-    // var time = $("the parent elements ID") 
+    $(this).parent().addClass("past");
+    
    
 } 
 
